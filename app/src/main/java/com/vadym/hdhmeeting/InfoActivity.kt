699a09fe76403748a15215912000a10318ba9848
Vadym <gvd@loyverse.com>
2024-11-21 00:33:45 +0200

    Add version to Info

--- app/src/main/java/com/vadym/hdhmeeting/InfoActivity.kt
+++ app/src/main/java/com/vadym/hdhmeeting/InfoActivity.kt
@@ -1,9 +1,14 @@
 package com.vadym.hdhmeeting
 
 import android.os.Bundle
+import android.widget.TextView
 
 class InfoActivity: BaseActivity() {
     override fun init(savedInstanceState: Bundle?) {
         super.setContentView(R.layout.view_info)
+
+        val version: TextView = findViewById(R.id.version)
+        version.text = "v. " + packageManager.getPackageInfo(packageName, 0).versionName
+
     }
 }
